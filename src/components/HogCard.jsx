093720986a9608bccb890fe7b53359ee1d401551
Ivan Szebenszki <szib77@gmail.com>
2019-06-06 12:41:35 +0100

    Add fridge ratio, greased to details

--- src/components/HogCard.jsx
+++ src/components/HogCard.jsx
@@ -12,18 +12,26 @@ export default class HogCard extends Component {
   }
 
   render() {
-    const picUrl = require('../hog-imgs/' + this.props.hog.name.toLowerCase().replace(/ /gi, '_') + '.jpg')
+    const { hog } = this.props
+    const picUrl = require('../hog-imgs/' + hog.name.toLowerCase().replace(/ /gi, '_') + '.jpg')
+    const ratio = hog['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water']
     return (
       <div className="ui raised card" onClick={this.toggle}>
         <div className="image">
-          <img src={picUrl} />
+          <img src={picUrl} alt={hog.name} />
         </div>
         <div className="content">
-          <a className="header">{this.props.hog.name}</a>
+          <a className="header">{hog.name}</a>
         </div>
         {
           this.state.withDetails &&
-          <div>Medal:{this.props.hog['highest medal achieved']}</div>
+          <div className='extra content'>
+            <div className="text-bold">Medal: {hog['highest medal achieved']}</div>
+            <hr />
+            <div>Weight ratio to some random fridge: {ratio}</div>
+            <hr />
+            <div>{hog.greased ? 'Greased' : 'Not greased'}</div>
+          </div>
         }
       </div>
     )
